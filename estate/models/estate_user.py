2fb0850fedcb9b2dfe28992a2eca30e627b2c5eb
Ahmad (ahmn) <ahmn@odoo.com>
2025-05-05 10:40:24 +0400

    Delete estate directory

--- estate/models/estate_user.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-class EstateUser(models.Model):
-    _inherit = 'res.users'
-    _description = "bla"
-    
-    property_ids = fields.One2many('estate.property', 'salesperson', domain=['|', ('state','=','new'), ('state','=','offer_received')])
